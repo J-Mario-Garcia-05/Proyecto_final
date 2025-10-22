8e834834e5ad9335400ffad2ef547158435f2b12
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-10-21 22:38:04 -0600

    Tabla tareas

--- OperGest.py
+++ OperGest.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 DB_NAME = "registros.db"
 conn = sqlite3.connect(DB_NAME)
@@ -60,6 +61,8 @@ class bandos:
         cantidad INTEGER NOT NULL,
         FOREIGN KEY (corte) REFERENCES pedidos (id))
         ''')
+        conn.commit()
+        return conn
 
     def agregar_bando(self):
         conn.execute(
@@ -105,11 +108,19 @@ class Operaciones:
         big_price  REAL NOT NULL)
         ''')
         conn.commit()
+        return conn
+
+    def guardar(self):
+        with self.__conn() as cursor:
+            cursor.execute(
+                "INSERT INTO operaciones (nombre, small_price, big_price) VALUES (?, ?, ?, ?)",
+                (self.nombre, self.small_price, self.big_price)
+            )
 
     @staticmethod
-    def modificar(nombre):
-        with Pedidos._conn() as cursor:
-            cur = cursor.execute('SELECT * FROM operaciones WHERE nombre = ?', (nombre,))
+    def modificar(id):
+        with Operaciones._conn() as cursor:
+            cur = cursor.execute('SELECT * FROM operaciones WHERE id = ?', (id,))
             fila = cur.fetchone()
             if not fila:
                 raise ValueError("No se encontró ninguna operación con ese nombre!")
@@ -118,18 +129,18 @@ class Operaciones:
             big_price = input(f"Actualziar precio talla grande [{fila['big_price']}]: ") or fila['big_price']
             conn.execute(
                 "UPDATE pedidos SET nombre, small_price, big_price = ? WHERE id = ?",
-                (nombre, small_price, big_price, fila['id'])
+                (nombre, small_price, big_price, id)
             )
 
     @staticmethod
-    def eliminar(nombre):
-        with Pedidos._conn() as cursor:
-            cur = cursor.execute('DELETE FROM operaciones WHERE nombre = ?', (nombre,))
+    def eliminar(id):
+        with Operaciones._conn() as cursor:
+            cur = cursor.execute('DELETE FROM operaciones WHERE nombre = ?', (id,))
             if cur.rowcount:
                 raise ValueError("No se encontró ninguna operación con el nombre ingresado!")
 
 
-class Empleado:
+class Empleados:
     def __init__(self, nombre, telefono, area):
         self.nombre = nombre
         self.telefono = telefono
@@ -145,10 +156,28 @@ class Empleado:
         telefono INTEGER NOT NULL,
         area TEXT NOT NULL)
                      ''')
+        conn.commit()
+        return conn
+
+    def guardar(self):
+        with self.__conn() as cursor:
+            cursor.execute(
+                "INSERT INTO empleados (nombre, telefono, area) VALUES (?, ?, ?)"
+                (self.nombre, self.telefono, self.area)
+            )
+
+    @staticmethod
+    def listar():
+        with Empleados.__conn() as cursor:
+            cur = cursor.execute('SELECT * FROM empleados')
+            lista = cur.fetchall()
+            if not lista:
+                raise ValueError("No hay empleados registrados!")
+            print("\n--")
 
     @staticmethod
     def modificar(id):
-        with Pedidos._conn() as cursor:
+        with Empleados._conn() as cursor:
             cur = cursor.execute('SELECT * FROM empleados WHERE id = ?', (id,))
             fila = cur.fetchone()
             if not fila:
@@ -158,5 +187,62 @@ class Empleado:
             area = input(f"Actualizar area {fila['area']}: ") or fila['area']
             conn.execute(
                 "UPDATE empleados SET nombre, telefono, area = ? WHERE id = ?",
-                (nombre, telefono, fila['id'])
+                (nombre, telefono, area, id)
             )
+
+    @staticmethod
+    def eliminar(id):
+        with Empleados.__conn() as cursor:
+            cur = cursor.execute('DELETE FROM empleados WHERE id = ?', (id,))
+            if cur.rowcount:
+                raise ValueError("No se encontró ningún empleado!")
+
+class Tareas:
+    def __init__(self, id_empleado, corte, bandos, operacion, fecha):
+        self.id_empleado = id_empleado
+        self.corte = corte
+        self.bandos = bandos
+        self.operacion = operacion
+
+    @staticmethod
+    def __conn():
+        conn.row_factory = sqlite3.Row
+        conn.execute('''
+        CREATE TABLE IF NOT EXISTS tareas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_empleado INTEGER NOT NULL,
+        corte INTEGER NOT NULL,
+        bandos INTEGER NOT NULL,
+        operacion INTEGER NOT NULL
+        FOREIGN KEY(id_empleado) REFERENCES empleados(id),
+        FOREIGN KEY(corte) REFERENCES pedidos(id),
+        FOREIGN KEY(bandos) REFERENCES bandos(id),
+        FOREIGN KEY(operacion) REFERENCES operaciones(id))
+        ''')
+        conn.commit()
+        return conn
+
+    def guardar(self):
+        with self.__conn() as cursor:
+            cursor.execute(
+                "INSERT INTO tareas(id_empleado, corte, bandos, operacion) VALUES (?, ?, ?, ?)",
+                (self.id_empleado, self.corte, self.bandos, self.operacion)
+            )
+
+    @staticmethod
+    def listar(id_empleado):
+        with Tareas.__conn() as cursor:
+            lista = cursor.execute('SELECT * FROM tareas WHERE id_empleado = id_empleado').fetchall()
+            if not lista:
+                raise ValueError("No se le han asignado tareas aún.")
+            print("\n--")
+
+    @staticmethod
+    def editar(id_empleado):
+        with Tareas.__conn() as cursor:
+            fila = cursor.execute("SELECT FROM tareas WHERE id_empleado = ?", (id_empleado)).fetchone()
+            if not fila:
+                raise ValueError("Aún no se le han asignado tareas")
+            print("Listar las tareas con un botón de modificar....")
+            fila = cursor.execute("SELECT FROM tareas WHERE id_empleado = ? AND id = ?",
+                                  (id_empleado, id)).fetchone()
